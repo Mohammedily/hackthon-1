b9f4d53f2d6e89977852703cf853444ba5a76ce2
mohammed ilyas <MY_NAME@example.com>
2021-12-29 14:34:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+ fetch("https://cataas.com/api/cats?tags=cute")
+ .then((data) =>{
+     return data.json();
+ })
+ .then((completedata)=>{
+     let data1="";
+     completedata.map((values) => {
+         data1+=`<div>
+         <img src=${values.tags} alt="img" class="img">
+         </div>
+         
+         `;
+     })
+   console.log(data1);
+   document.getElementById("demo").innerHTML = data1
+ })
+ .catch((err) =>{
+     console.log(err);
+ })
